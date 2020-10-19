266b008b7c75572743ba42188fed5a7205a2c312
Константин Худышка <k.iroshnikov@itmegastar.com>
2020-10-19 19:58:17 +0700

    Добавлены тестовые примеры

--- CalendarMonthExample/ViewController.swift
+++ CalendarMonthExample/ViewController.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collection: CollectionCalendar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let dataParser = DateParser()
+        dataParser.dateFrom = Date(day: 1, month: 1, year: 2019)
+        dataParser.dateTo = Date(day: 31, month: 12, year: 2021)
+        
+        collection.dataParser = dataParser
+        collection.blockTextHeader = {[weak self] text in
+            self?.title = text
+        }
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        collection.scrollCollection()
     }
 
 
