580e787b8222c3e7d5a71461f7a9a7a18692d502
Константин Худышка <k.iroshnikov@itmegastar.com>
2020-10-19 19:42:56 +0700

    Начальная настройка проекта

---
+++ CalendarMonth/CalendarMonth.h
@@ -0,0 +1,18 @@
+//
+//  CalendarMonth.h
+//  CalendarMonth
+//
+//  Created by Админ on 19.10.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CalendarMonth.
+FOUNDATION_EXPORT double CalendarMonthVersionNumber;
+
+//! Project version string for CalendarMonth.
+FOUNDATION_EXPORT const unsigned char CalendarMonthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalendarMonth/PublicHeader.h>
+
+
---
+++ CalendarMonthExample/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  CalendarMonthExample
+//
+//  Created by Админ on 19.10.2020.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+
+}
+
